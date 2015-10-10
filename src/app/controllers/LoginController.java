5c9f253c406a0b5bfa31cb9cc1c9855d157b3efc
Arin Houck <arinhouck@gmail.com>
2015-10-10 00:31:33 -0700

    controller to view with action handler on button

--- src/app/controllers/LoginController.java
+++ src/app/controllers/LoginController.java
@@ -1,14 +1,30 @@
 package app.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
+    @FXML
+    private BottomBarController bottomBarController;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View loaded.");
+    }
+
+    @FXML
+    protected void handleLoginAction(ActionEvent actionEvent) {
+        // Handle Login
     }
 }
