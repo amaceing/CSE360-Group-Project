7340e7699a3a0ff2d61252a67027c6be8acdf76b
Arin Houck <arinhouck@gmail.com>
2015-10-10 00:31:00 -0700

    setup driver with string properties

--- src/app/models/Driver.java
+++ src/app/models/Driver.java
@@ -6,12 +6,40 @@ import javafx.beans.property.SimpleStringProperty;
  */
 public class Driver {
 
+    private final SimpleStringProperty firstName;
+    private final SimpleStringProperty lastName;
     private final SimpleStringProperty username;
     private final SimpleStringProperty password;
 
-    public Driver(String username, String password) {
-        this.username =  new SimpleStringProperty(username);
-        this.password =  new SimpleStringProperty(password);
+    public Driver(String firstName, String lastName, String username, String password) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.username = new SimpleStringProperty(username);
+        this.password = new SimpleStringProperty(password);
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
     }
 
     public String getUsername() {
