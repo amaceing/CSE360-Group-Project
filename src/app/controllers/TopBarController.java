bc62ac6facd0261b9a89e9cd8ebe1958efa8bff7
Arin Houck <arinhouck@gmail.com>
2015-10-09 23:24:49 -0700

    initial project setup

---
+++ src/app/controllers/TopBarController.java
@@ -0,0 +1,29 @@
+package app.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by arinhouck on 10/9/15.
+ */
+public class TopBarController implements Initializable {
+
+    @FXML
+    private Label time;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Set time eg. 12:00 PM
+        DateFormat currentTime = new SimpleDateFormat("hh:mm a");
+        Date date = new Date();
+        time.setText(currentTime.format(date).toString());
+    }
+}
