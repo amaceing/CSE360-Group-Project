717a1a59468cafe3e65c551596677f3c3e170169
Arin Houck <arinhouck@gmail.com>
2015-10-17 12:53:47 -0700

    start of transition to radio view with controller

---
+++ src/app/controllers/RadioController.java
@@ -0,0 +1,16 @@
+package app.controllers;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by arinhouck on 10/17/15.
+ */
+public class RadioController  implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+}
