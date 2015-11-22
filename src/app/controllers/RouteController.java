dc857b927366f4f916fe05638140835fdbea7815
Drew Davis <drewdavis888@gmail.com>
2015-11-22 14:10:13 -0700

    route testing

--- src/app/controllers/RouteController.java
+++ src/app/controllers/RouteController.java
@@ -21,14 +21,48 @@ public class RouteController implements Initializable {
     @FXML
     private Slider slider;
 
+    private double milesRemaining;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         topBarController.setBackButton(VistaNavigator.DASHBOARD, RouteController.this);
         mainController = VistaNavigator.getMainController();
 
-        slider.setValue(Main.MAX_MILEAGE - mainController.getSession().getDriver().getMilesRemaining());
-        slider.setMin(0);
-        slider.setMax(Main.MAX_MILEAGE);
+        setMilesRemaining(mainController.getSession().getDriver().getMilesRemaining());
+        initSlider(0, Main.MAX_MILEAGE, milesRemaining);
+    }
+
+    public double initSlider(int min, int max, double milesRemaining) {
+        double value = max - milesRemaining;
+        slider.setValue(value);
+        slider.setMin(min);
+        slider.setMax(max);
         slider.setDisable(true);
+
+        return value;
+    }
+
+    public Slider getSlider() {
+        return slider;
+    }
+
+    public void setSlider(Slider slider) {
+        this.slider = slider;
+    }
+
+    public static MainController getMainController() {
+        return mainController;
+    }
+
+    public static void setMainController(MainController mainController) {
+        RouteController.mainController = mainController;
+    }
+
+    public double getMilesRemaining() {
+        return milesRemaining;
+    }
+
+    public void setMilesRemaining(double milesRemaining) {
+        this.milesRemaining = milesRemaining;
     }
 }
